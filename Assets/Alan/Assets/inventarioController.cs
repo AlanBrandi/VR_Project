d184d48cb51d3884b5974f37c6049bdf2250fc70
AlanBrandi <alan.brandi@hotmail.com>
2023-05-26 15:17:26 -0300

    Add Playermovement + collectableSystem

---
+++ Assets/Alan/Assets/inventarioController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventarioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeIten(GameObject iten)
+    {
+        Debug.LogWarning("Item =" + iten.GetComponent<Iten>().id);
+    }
+}
---
+++ Assets/Alan/Scripts/CollectableManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectableManager : MonoBehaviour
+{
+   //Instance
+   public static CollectableManager Instance;
+   
+   [Header("Collectable Configuration")] 
+   public int collectableAmount;
+   public GameObject collectableFX;
+   public AudioSource colletableAudio;
+   
+   private int currentCollectables; 
+   
+   [Header("Collectable Data")]
+   [SerializeField] private CollectableSO collectableSo;
+   
+   [Header("Canvas")]
+   [SerializeField] private TMP_Text colletableTXT;
+
+   
+   private void Awake()
+   {
+      if (!Instance)
+      {
+         Instance = this;
+      }
+      else
+      {
+         Destroy(this.gameObject);
+      }
+
+      colletableAudio = GetComponent<AudioSource>();
+      currentCollectables = collectableSo.currentCollectable;
+      colletableTXT.text = currentCollectables.ToString();
+   }
+
+   public void AddCollectable()
+   {
+      currentCollectables += collectableAmount;
+      collectableSo.currentCollectable = currentCollectables;
+      colletableTXT.text = currentCollectables.ToString();
+   }
+}
