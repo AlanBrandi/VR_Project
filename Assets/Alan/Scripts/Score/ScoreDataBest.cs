760c6306673612c2049d720be6af1310585a63da
AlanBrandi <alan.brandi@hotmail.com>
2023-05-28 14:41:38 -0300

    add: BestScore save game

---
+++ Assets/Alan/Scripts/Score/ScoreDataBest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "bestScore", menuName = "New ScoreDataBest")]
+public class ScoreDataBest : ScriptableObject
+{
+   public float bestTime;
+   public float bestScore;
+}
